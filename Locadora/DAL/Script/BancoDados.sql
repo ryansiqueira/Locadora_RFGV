c5c7797aca9b5b002be85de96ce8073b041a1cb7
Felipe Negri da Silva <felipe_silva@esamcsorocaba.com.br>
2020-11-04 21:58:10 -0300

    Criação do banco de dados

---
+++ Locadora/DAL/Script/BancoDados.sql
@@ -0,0 +1,39 @@
+﻿CREATE DATABASE Locadora
+GO
+
+USE Locadora
+GO
+
+CREATE TABLE Funcionarios (
+    CdFuncionario INT PRIMARY KEY IDENTITY,
+	Nome     VARCHAR (50)  NOT NULL,
+    CPF        VARCHAR (14)  NOT NULL,
+    DtNascimento  DATE          NOT NULL,
+    DsEstadoCivil CHAR (1)      NOT NULL,
+    Sexo        CHAR (1)      NOT NULL,
+    Email       VARCHAR (100) NOT NULL,
+    Telefone  VARCHAR (20)  NOT NULL,
+    RG VARCHAR(10) NOT NULL,
+    Endereco varchar(MAX) NOT NULL,
+    Cidade varchar(MAX) NOT NULL,
+    CEP varchar(9) NOT NULL,
+    Estado varchar(3) NOT NULL,
+    Celular varchar(12) NOT NULL,
+    DsStatus varchar(max) NOT NULL
+)
+
+CREATE TABLE Itens (
+        CdItem INT PRIMARY KEY IDENTITY,
+        CodigoBarras INT NOT NULL,
+        Titulo varchar(50) NOT NULL,
+        Genero varchar(50) NOT NULL,
+        Ano date NOT NULL,
+        Tipo char(1) NOT NULL,
+        Preco decimal NOT NULL,
+        DtAdquirida date NOT NULL,
+        VlCusto decimal NOT NULL,
+        Situacao char(1) NOT NULL,
+        Atores varchar(max) NOT NULL,
+        Diretor varchar(max) NOT NULL,
+        CapaFilme image NOT NULL
+)

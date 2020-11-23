9aafa0e1a7ba01cddc36660f48aeddd816a08118
Guilherme Cleto Gomes <gcg.cleto@gmail.com>
2020-11-22 22:06:31 -0300

    alerações devolução

--- GUI/frmDevolucao.Designer.cs
+++ GUI/frmDevolucao.Designer.cs
@@ -34,19 +34,29 @@ namespace GUI
             this.txtValor = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.cbxPagamento = new System.Windows.Forms.ComboBox();
             this.btnBuscar = new System.Windows.Forms.Button();
             this.btnCadastrar = new System.Windows.Forms.Button();
             this.btnAlterar = new System.Windows.Forms.Button();
             this.btnExcluir = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtCodigo = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dtpDataEntrega = new System.Windows.Forms.DateTimePicker();
+            this.dtpDataPrevista = new System.Windows.Forms.DateTimePicker();
+            this.label6 = new System.Windows.Forms.Label();
+            this.Itens = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.FKCliente = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.CodigoLocacao = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.ckbRecebido = new System.Windows.Forms.CheckBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(24, 32);
+            this.label1.Location = new System.Drawing.Point(15, 54);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(133, 13);
             this.label1.TabIndex = 0;
@@ -54,14 +64,14 @@ namespace GUI
             // 
             // txtCodigoBarras
             // 
-            this.txtCodigoBarras.Location = new System.Drawing.Point(163, 29);
+            this.txtCodigoBarras.Location = new System.Drawing.Point(154, 51);
             this.txtCodigoBarras.Name = "txtCodigoBarras";
             this.txtCodigoBarras.Size = new System.Drawing.Size(82, 20);
             this.txtCodigoBarras.TabIndex = 1;
             // 
             // txtValor
             // 
-            this.txtValor.Location = new System.Drawing.Point(163, 55);
+            this.txtValor.Location = new System.Drawing.Point(154, 129);
             this.txtValor.Name = "txtValor";
             this.txtValor.Size = new System.Drawing.Size(82, 20);
             this.txtValor.TabIndex = 7;
@@ -69,7 +79,7 @@ namespace GUI
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(96, 58);
+            this.label4.Location = new System.Drawing.Point(87, 132);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(61, 13);
             this.label4.TabIndex = 6;
@@ -78,31 +88,23 @@ namespace GUI
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(93, 84);
+            this.label5.Location = new System.Drawing.Point(84, 158);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(64, 13);
             this.label5.TabIndex = 8;
             this.label5.Text = "Pagamento:";
             // 
-            // dataGridView1
-            // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(5, 150);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(335, 180);
-            this.dataGridView1.TabIndex = 10;
-            // 
             // cbxPagamento
             // 
             this.cbxPagamento.FormattingEnabled = true;
-            this.cbxPagamento.Location = new System.Drawing.Point(163, 81);
+            this.cbxPagamento.Location = new System.Drawing.Point(154, 155);
             this.cbxPagamento.Name = "cbxPagamento";
             this.cbxPagamento.Size = new System.Drawing.Size(82, 21);
             this.cbxPagamento.TabIndex = 11;
             // 
             // btnBuscar
             // 
-            this.btnBuscar.Location = new System.Drawing.Point(251, 23);
+            this.btnBuscar.Location = new System.Drawing.Point(242, 45);
             this.btnBuscar.Name = "btnBuscar";
             this.btnBuscar.Size = new System.Drawing.Size(89, 31);
             this.btnBuscar.TabIndex = 12;
@@ -111,7 +113,7 @@ namespace GUI
             // 
             // btnCadastrar
             // 
-            this.btnCadastrar.Location = new System.Drawing.Point(29, 336);
+            this.btnCadastrar.Location = new System.Drawing.Point(39, 417);
             this.btnCadastrar.Name = "btnCadastrar";
             this.btnCadastrar.Size = new System.Drawing.Size(89, 31);
             this.btnCadastrar.TabIndex = 13;
@@ -120,7 +122,7 @@ namespace GUI
             // 
             // btnAlterar
             // 
-            this.btnAlterar.Location = new System.Drawing.Point(124, 336);
+            this.btnAlterar.Location = new System.Drawing.Point(134, 417);
             this.btnAlterar.Name = "btnAlterar";
             this.btnAlterar.Size = new System.Drawing.Size(89, 31);
             this.btnAlterar.TabIndex = 14;
@@ -129,18 +131,115 @@ namespace GUI
             // 
             // btnExcluir
             // 
-            this.btnExcluir.Location = new System.Drawing.Point(219, 336);
+            this.btnExcluir.Location = new System.Drawing.Point(229, 417);
             this.btnExcluir.Name = "btnExcluir";
             this.btnExcluir.Size = new System.Drawing.Size(89, 31);
             this.btnExcluir.TabIndex = 15;
             this.btnExcluir.Text = "Excluir";
             this.btnExcluir.UseVisualStyleBackColor = true;
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(105, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "Código:";
+            // 
+            // txtCodigo
+            // 
+            this.txtCodigo.Location = new System.Drawing.Point(154, 24);
+            this.txtCodigo.Name = "txtCodigo";
+            this.txtCodigo.Size = new System.Drawing.Size(82, 20);
+            this.txtCodigo.TabIndex = 17;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(60, 109);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(88, 13);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "Data de Entrega:";
+            // 
+            // dtpDataEntrega
+            // 
+            this.dtpDataEntrega.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpDataEntrega.Location = new System.Drawing.Point(154, 103);
+            this.dtpDataEntrega.Name = "dtpDataEntrega";
+            this.dtpDataEntrega.Size = new System.Drawing.Size(82, 20);
+            this.dtpDataEntrega.TabIndex = 19;
+            // 
+            // dtpDataPrevista
+            // 
+            this.dtpDataPrevista.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpDataPrevista.Location = new System.Drawing.Point(154, 77);
+            this.dtpDataPrevista.Name = "dtpDataPrevista";
+            this.dtpDataPrevista.Size = new System.Drawing.Size(82, 20);
+            this.dtpDataPrevista.TabIndex = 21;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(74, 83);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(74, 13);
+            this.label6.TabIndex = 20;
+            this.label6.Text = "Data Prevista:";
+            // 
+            // Itens
+            // 
+            this.Itens.DataPropertyName = "CdItens";
+            this.Itens.HeaderText = "Filmes";
+            this.Itens.Name = "Itens";
+            // 
+            // FKCliente
+            // 
+            this.FKCliente.DataPropertyName = "FKCliente";
+            this.FKCliente.HeaderText = "Nome Cliente";
+            this.FKCliente.Name = "FKCliente";
+            // 
+            // CodigoLocacao
+            // 
+            this.CodigoLocacao.DataPropertyName = "CdLocacao";
+            this.CodigoLocacao.HeaderText = "Codigo Locação";
+            this.CodigoLocacao.Name = "CodigoLocacao";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.CodigoLocacao,
+            this.FKCliente,
+            this.Itens});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 231);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(335, 180);
+            this.dataGridView1.TabIndex = 10;
+            // 
+            // ckbRecebido
+            // 
+            this.ckbRecebido.AutoSize = true;
+            this.ckbRecebido.Location = new System.Drawing.Point(87, 208);
+            this.ckbRecebido.Name = "ckbRecebido";
+            this.ckbRecebido.Size = new System.Drawing.Size(72, 17);
+            this.ckbRecebido.TabIndex = 22;
+            this.ckbRecebido.Text = "Recebido";
+            this.ckbRecebido.UseVisualStyleBackColor = true;
+            // 
             // frmDevolucao
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(344, 376);
+            this.ClientSize = new System.Drawing.Size(360, 460);
+            this.Controls.Add(this.ckbRecebido);
+            this.Controls.Add(this.dtpDataPrevista);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.dtpDataEntrega);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtCodigo);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.btnExcluir);
             this.Controls.Add(this.btnAlterar);
             this.Controls.Add(this.btnCadastrar);
@@ -167,11 +266,21 @@ namespace GUI
         private System.Windows.Forms.TextBox txtValor;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.ComboBox cbxPagamento;
         private System.Windows.Forms.Button btnBuscar;
         private System.Windows.Forms.Button btnCadastrar;
         private System.Windows.Forms.Button btnAlterar;
         private System.Windows.Forms.Button btnExcluir;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtCodigo;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DateTimePicker dtpDataEntrega;
+        private System.Windows.Forms.DateTimePicker dtpDataPrevista;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Itens;
+        private System.Windows.Forms.DataGridViewTextBoxColumn FKCliente;
+        private System.Windows.Forms.DataGridViewTextBoxColumn CodigoLocacao;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.CheckBox ckbRecebido;
     }
 }
